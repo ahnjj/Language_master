944ad9e2ddd42e1e18ffdc34f04300fe5e77f70f
jeong_ahn <ahnjj.dev@gmail.com>
2023-09-25 23:19:59 +0900

    index.html added, base.html edited 0925

--- langapp/urls.py
+++ langapp/urls.py
@@ -3,12 +3,11 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name="langapp/vocab_list.html"),name = 'vocab_list'), # 단어 전체목록
-    path('', views.vocab_list, name = 'vocab_list'),  # 단어 전체 목록
-    path('<int:pk>/', views.vocab_detail, name = 'vocab_detail'),  # 개별 단어 상세 조회
-    # path('post/', TemplateView.as_view(template_name="langapp/vocab_post.html")), # 단어 등록
+    path('', TemplateView.as_view(template_name="langapp/index.html")), # 단어 전체목록
+    path('list/', views.vocab_list, name = 'vocab_list'),  # 단어 전체 목록
+    path('list/<int:pk>/', views.vocab_detail, name = 'vocab_detail'),  # 개별 단어 상세 조회
     path('post/', views.vocab_post, name='vocab_post'),  # 단어 등록
-    path('<int:pk>/edit/',views.vocab_edit, name = 'vocab_edit'),  # 수정
+    path('list/<int:pk>/edit/',views.vocab_edit, name = 'vocab_edit'),  # 수정
     path('complete/', views.complete_list, name='complete_list'),  # 완료 목록
     path('complete/<int:pk>/', views.completed, name='completed'),  # 완료 체크
     path('uncomplete/<int:pk>/', views.uncompleted, name='uncompleted'),  # 미완료 체크
